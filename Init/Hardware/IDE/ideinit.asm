4c1dc7159b0cfdb63d6a7010ddd0fa529ca22938
Yll Buzoku <ybuzoku@hotmail.co.uk>
2022-04-18 22:50:35 +0100

    Moved an IDE proc from resident to init

--- Init/Hardware/IDE/ideinit.asm
+++ Init/Hardware/IDE/ideinit.asm
@@ -1,4 +1,6 @@
-ideInitialisation:
+
+IDE:
+.ideInitialisation:
 ;Check primary and secondary bus for master and slave drives
 ; Maximum of 4 "fixed" ATA drives
 ;Due to being in compatibility mode, they respond to 
@@ -25,5 +27,35 @@ ideInitialisation:
 
     jmp short .ideInitEnd
 .bad:
+    jmp short .ideInitEnd
+;===========================
+;     Callable procs       :
+;===========================
+.addControllerTable:
+;Adds a PCI IDE controller to the internal data tables, if there is space
+; If there is no space, returns with carry set.
+;Input: eax = BAR5 address
+;       ebx = PCI IO address
+;Output: CF=NC, all ok, CF=CY, device not added.
+    push rsi
+    cmp byte [ideNumberOfControllers], 2
+    je .actfail ;If it is 2, fail
+    inc byte [ideNumberOfControllers]
+    mov rsi, ideControllerTable
+    cmp byte [rsi], 0   ;Is the first entry empty?
+    jz .act0    ;If yes, write entry
+    add rsi, ideTableEntrySize  ;Else, goto second entry space
+.act0:
+    mov dword [rsi], ebx    ;Move first PCI IO addr
+    mov byte [rsi], 0       ;Zero the register index
+    mov dword [rsi + 4], eax    ;Move next data
+    clc
+.actexit:
+    pop rsi
+    ret
+.actfail:
+    stc
+    jmp short .actexit
 
+;Must be the end of the file
 .ideInitEnd:
