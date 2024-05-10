d6b603ab393e20165e57e3be7cf7ab562345d129
ybuzoku <ybuzoku@hotmail.co.uk>
2024-05-10 03:06:18 +0100

    Updated the BIOS date

--- Source/Resident/Misc/version.asm
+++ Source/Resident/Misc/version.asm
@@ -1,4 +1,4 @@
 ;----------------------------------------------------------------
 signature:    db "v0.9 SCPBIOS"    ;12 byte signature
 signature2:   db "Copyright (C) Yll Buzoku"
-              db "03/12/2021"
+              db "10/05/2024"
